608e276a352c48823854227e55f9208a74b99a67
Sandeep Yadav <Yadav.sandeep@primthon.in>
2024-04-10 10:49:12 +0530

    added welcome message

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,25 +1,34 @@
-import { Body, Controller, Get, Post, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { userServices } from './user.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth-guards';
 
 @Controller('user')
 export class UserController {
-  constructor(
-    private readonly userServices : userServices,
- ){}
+  constructor(private readonly userServices: userServices) {}
 
- @Get()
-//  @UseGuards(JwtAuthGuard)
+  @Get()
+  //  @UseGuards(JwtAuthGuard)
   async get() {
-    return  await this.userServices.getUsers();
+    return await this.userServices.getUsers();
   }
 
+  @Get('cicd')
+  async welcome() {
+    return 'Welcome to cicd pipeline';
+  }
 
   @Post('upload')
   @UseInterceptors(FileInterceptor('file'))
   uploadFile(@UploadedFile() file: Express.Multer.File) {
-    console.log(file,"======");
+    console.log(file, '======');
   }
-
 }
